647caa925983be3561ddc6977e16b9fe7faa0d41
Aymen <labidiaymen@outlook.com>
2019-01-24 16:48:13 +0100

    feat: add loopback/next

---
+++ api/test/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {AdminApi} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new AdminApi({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: AdminApi;
+  client: Client;
+}
