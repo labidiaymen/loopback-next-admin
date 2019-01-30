1001b2d8c2da7f34af62034734e060e79d459089
Aymen <labidiaymen@outlook.com>
2019-01-30 16:56:31 +0100

    feat(layout): dashboard

--- client/src/app/@modules/dashboard/dashboard.component.ts
+++ client/src/app/@modules/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { authenticated, Authentication } from '@sdk/authentication';
+import { LabRouter } from '@sdk/lab-router';
+import { GlobalRoute } from '@sdk/default-router';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,11 +12,26 @@ import { Router } from '@angular/router';
 export class DashboardComponent implements OnInit {
 
   constructor(private router: Router) { }
+
   href = '';
+  me: any = {};
   breadcrumb: string[];
-  ngOnInit() {
+
+  /**
+   * @returns void
+   */
+  ngOnInit(): void {
+    Authentication.check();
+    authenticated.subscribe(me => this.me = me);
     this.href = this.router.url;
     this.breadcrumb = this.href.split('/');
+  }
+
+  /**
+   * @returns void
+   */
+  onLogout(): void {
+    Authentication.logout();
   }
 
 }
