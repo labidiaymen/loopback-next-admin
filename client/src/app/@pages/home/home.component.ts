44cc56f62326c471be580b59605e5f457a9cb079
Aymen <labidiaymen@outlook.com>
2019-01-30 12:07:06 +0100

    chore: load connected user in the home page

--- client/src/app/@pages/home/home.component.ts
+++ client/src/app/@pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LBStorage } from '@sdk/lb-storage';
+import { HOST } from '@sdk/config';
+import { Resuqets } from '@sdk/requests';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +9,14 @@ import { LBStorage } from '@sdk/lb-storage';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  me: any;
   constructor() { }
 
   ngOnInit() {
-    console.log(LBStorage.loadToken());
+    Resuqets.get(`${HOST}/users/me`).subscribe(result => {
+      console.log(result);
+      this.me = result;
+    });
   }
 
 }
