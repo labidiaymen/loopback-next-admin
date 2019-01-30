eeb1e3fbf54b18f531e5332da285d0247ec9a11e
Aymen <labidiaymen@outlook.com>
2019-01-30 16:55:37 +0100

    feat(auth): add checking system

--- client/@sdk/authentication.ts
+++ client/@sdk/authentication.ts
@@ -1,13 +1,26 @@
 import { Resuqets } from './requests';
 import { HOST } from './config';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { LBStorage } from './lb-storage';
 import { LabRouter } from './lab-router';
 import { GlobalRoute } from './default-router';
 
+export const authenticated = new BehaviorSubject<any>({});
 export class Authentication {
   constructor(private resuqets: Resuqets) {
 
+  }
+
+  /**
+   * @returns Promise
+   */
+  static async check(): Promise<any> {
+    const me = await Resuqets.get(`${HOST}/users/me`).toPromise();
+    if ('error' in me) {
+      LabRouter.next(GlobalRoute.Login);
+    } else {
+      authenticated.next(me);
+    }
   }
 
   /**
