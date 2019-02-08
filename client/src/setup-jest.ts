407dc53983f68df0d3a1e1e6f37904a74f2abd37
Aymen <labidiaymen@outlook.com>
2019-02-08 11:15:49 +0100

    chore: remove chore

--- client/src/setup-jest.ts
+++ client/src/setup-jest.ts
@@ -1,10 +1,5 @@
 import 'jest-preset-angular';
 declare var global;
-const { Response, Request, Headers, fetch } = require('whatwg-fetch');
-// global.Response = Response;
-// global.Request = Request;
-// global.Headers = Headers;
-// global.fetch = fetch;
 const xhrMockClass = () => ({
   open: jest.fn(),
   send: jest.fn(),
